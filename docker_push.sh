f33de625face86221c9b96c94b059a5441cda411
Julian Riegraf <julianriegraf@web.de>
2020-06-22 20:52:25 +0200

    bugfixes

--- docker_push.sh
+++ docker_push.sh
@@ -11,5 +11,8 @@ docker build -t rummikub/game .
 cd "$project_root"/player || exit
 docker build -t rummikub/player .
 
-docker push rummikub/game
-docker push rummikub/player
+docker tag rummikub/player registry.heroku.com/rummikub-sa/player
+docker push registry.heroku.com/rummikub-sa/player
+
+docker tag rummikub/game registry.heroku.com/rummikub-sa/game
+docker push registry.heroku.com/rummikub-sa/game
